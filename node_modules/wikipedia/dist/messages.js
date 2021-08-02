9d1ea10cdd2c0e2aa9452c809e2536dd05f2e6c4
exoad <error666jm@gmail.com>
2021-08-01 22:08:47 -0400

    ignore

--- node_modules/wikipedia/dist/messages.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MSGS = void 0;
-exports.MSGS = {
-    PAGE_NOT_SUGGEST: 'No page with given title suggested : ',
-    PAGE_NOT_EXIST: 'No page with given title exists : ',
-    INFOBOX_NOT_EXIST: 'Info cannot be parsed for given page'
-};
