f383f8504042efcb031cef546aed81e63475073f
Jack Meng <error666jm@gmail.com>
2021-09-02 19:02:39 -0400

    remake

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const { prefix } = require('./configs/token.json');
+const { prefix } = require('../configs/token.json');
 
 app.get("/", (req, res) => res.send("Online."));
 
