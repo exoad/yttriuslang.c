4df4f02fa23f71494e094f2bff792b852f156996
Jack Meng <error666jm@gmail.com>
2021-09-03 07:57:30 -0400

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,4 +1,5 @@
 const { readdirSync } = require(`fs`);
+const moment = require("moment");
 
 module.exports = (bot) => {
     const load = dirs => {
@@ -7,6 +8,7 @@ module.exports = (bot) => {
             let pull = require(`../commands/${dirs}/${file}`);
             bot.commands.set(pull.config.name, pull);
             if (pull.config.aliases) pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
+            //add moment load time
             console.log(`Command | ${pull.config.name} has loaded`)
         }
     };
