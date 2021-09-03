987912315d28d10a9b928a2c9f7c253e80503d7b
Jack Meng <error666jm@gmail.com>
2021-09-03 16:44:14 -0400

    remake

--- commands/Main/docs.js
+++ commands/Main/docs.js
@@ -4,6 +4,7 @@ const chnl = require("../../configs/chnl.json");
 const resource = require("../../configs/resource.json");
 const superagent = require("superagent");
 const colors = require("../../configs/colors.json");
+const token = require("../../configs/token.json");
 module.exports = {
     config: {
         name: `docs`,
@@ -13,60 +14,25 @@ module.exports = {
     },
     run: async(bot, message, args) => {
         try {
-            let choice = args[0];
-
-            if (!args[0] || args[0] == undefined) {
+            let file_type = args[0];
+            if (!file_type || file_type == undefined || file_type == null) {
                 const embed = new MessageEmbed()
-                    .setTitle("Documentations and Extra Notations")
-                    .setDescription(
-                        "You can find extra info here that couldn't make it into the command itself. Check with the command to search for the valid `{tags}`"
-                    )
-                    .addField("Valid Tags", "```global, bot, service```")
-                    .setFooter("Confused? Join the support server!")
-                    .setThumbnail(resource.docs);
-
+                    .setTitle("Oops, this command requires some input arguments to proceed!")
+                    .setDescription("This command requires arguments after the inital command in order to give you the correct result!")
+                    .addField("Command Usage", "```" + token.prefix + "docs [file_type]```")
+                    .addField("file_type", "**Description:** The file type specified to view the documentations\n**Avaliable Parameters:** `md, markdown, txt, text, html`")
+                    .addField("Command Description", "Gets the documentation for this service.")
+                    .addField("Command Example Usage", "```" + token.prefix + "docs md```")
+                    .setThumbnail(resource.aw_snap)
+                    .setColor(colors.error);
                 message.channel.send({ embed });
-            } else if (args[0] == "bot") {
-                const embed = new MessageEmbed()
-                    .setTitle("Bot Documentations")
-                    .setDescription("Sci-Bot is a bot created by user: `exoad`")
-                    .addField(
-                        "Usage",
-                        "This bot's main purpose is of course to give information related to scientific topics."
-                    )
-                    .addField(
-                        "News",
-                        "This bot is still in pre-alpha is always being updated with new features"
-                    )
-                    .addField(
-                        "Features",
-                        `Prefix: ${config.prefix}\nThis bot is not designed to multitask and perform things outside of its tasks`
-                    )
-                    .addField("Extra Notes", "All Credits moved to `service` docs")
-                    .setThumbnail(resource.docs);
-
-                message.channel.send({ embed });
-            } else if (args[0] == "service") {
-                const embed = new MessageEmbed()
-                    .setTitle("Services and Credits")
-                    .addField(
-                        "APIs Used",
-                        "[NasaAPOD](https://apod.nasa.gov/apod/astropix.html)\n[RandomFact](https://fungenerators.com/api/facts/)\n[DiscordBotLib](https://github.com/yourWaifu/sleepy-discord)\n[LaTeX](https://github.com/latex3/)\n*Note most other APIs are either self made or private projects*"
-                    )
-                    .setThumbnail(resource.docs);
-
-                message.channel.send({ embed });
-            } else {
-                const embed = new MessageEmbed()
-                    .setTitle("Documentations and Extra Notations")
-                    .setDescription(
-                        "You can find extra info here that couldn't make it into the command itself. Check with the command to search for the valid `{tags}`"
-                    )
-                    .addField("Valid Tags", "```global, bot, service```")
-                    .setFooter("Confused? Join the support server!")
-                    .setThumbnail(resource.docs);
-
-                message.channel.send({ embed });
+            } else if (file_type == "md" || file_type == "markdown") {
+                message.channel.send("Fetching documentations for type `MARKDOWN`...").then(m => {
+                    setTimeout(function() {
+                        m.edit("Fetched it successfully!")
+                        message.channel.send({ file: "../../lib/archives/README.md" });
+                    }, 2000)
+                })
             }
         } catch (err) {
             const embed = new MessageEmbed()
