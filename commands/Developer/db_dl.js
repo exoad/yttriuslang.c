0f2b0acac613dd83ed23ad050e034f8a8a6fad15
Jack Meng <error666jm@gmail.com>
2021-09-07 13:42:36 -0400

    added a get header to the dl

--- commands/Developer/db_dl.js
+++ commands/Developer/db_dl.js
@@ -1,6 +1,8 @@
 const Database = require("easy-json-database");
 const { MessageEmbed, MessageAttachment } = require("discord.js");
 const config = require("../../configs/token.json");
+const resource = require("../../configs/resource.json");
+const moment = require("moment");
 module.exports = {
   config: {
     name: `dl`,
@@ -15,19 +17,27 @@ module.exports = {
       if (message.author.id != config.owner_id) return;
       else {
         if (args[0] == "clear") {
-        
           db.clear();
           message.channel.send("Cleared");
         } else if (args[0] == "all") {
-          if(!db.all()) return;
+          if (!db.all()) return;
           else {
             const dl = new MessageAttachment("db/registry_user.json");
             bot.channels.cache.get(config.side_channel).send(dl);
             message.channel.send("Informations sent to logs");
           }
+        } else if (args[0] == "get") {
+          let id = args[1];
+          if (!db.has(id)) message.channel.send("Failed to retrieve");
+          else {
+            let obj = JSON.stringify(db.get(id));
+            message.channel.send("Informations sent with coverage");
+            bot.channels.cache.get(config.side_channel).send("Requested "+  moment().format('MMMM Do YYYY, h:mm:ss a') + " " + obj);
+          }
         }
       }
     } catch (err) {
+      console.error(err);
       const embed = new MessageEmbed()
         .setTitle("Whoops, looks like something went wrong!")
         .setThumbnail(resource.aw_snap)
