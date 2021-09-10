a6fadd95a5ba840a67d4df93f2b2147e7988a1cd
Jack Meng <error666jm@gmail.com>
2021-09-10 15:50:03 -0400

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env
+#
+# Run and Daemon Launch Versions
+#
 
 
 echo -e "\e[34mStarting Daemon..." ; echo ;
@@ -5,7 +9,7 @@ echo -e "\e[34mStarting Daemon..." ; echo ;
 while :
 do	
 	node app.js
-	git pull https://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:x-oauth-basic@github.com/exoad/yAPI
+	git pull https://github.com/exoad/yAPI # Note the SSH & GPG keys are already set this means if you are going to use this, you must set that manually
   	echo -e "\e[31mPulled new updates..." ; echo ;
 	sleep 5
 done
