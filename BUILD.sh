26a4cc4e0d715ecc9a7b484fe8671dfa63e17bb3
Jack Meng <error666jm@gmail.com>
2021-09-10 16:07:24 -0400

    Update BUILD.sh

--- BUILD.sh
+++ BUILD.sh
@@ -5,6 +5,18 @@
 
 cd ~/.yAPI
 
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io
+
+echo ; echo -e "\e[32mDocker in check... " ; echo ;
+
 sudo docker build -t yapi .
 
 echo -e "\e[31mFinished building " ; echo -e "\e[34mRunning the Docker... " ; echo ;
