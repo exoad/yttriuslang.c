87edda1e54cb803588c2053426455f55e90923f4
Jack Meng <error666jm@gmail.com>
2021-09-10 15:33:49 -0400

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,7 +1,8 @@
-node .
+echo -e "\e[34mStarting Daemon..." ; echo ;
 
 while :
-do
+do	
+	node app.js
 	git pull https://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:x-oauth-basic@github.com/exoad/yAPI
   echo -e "\e[31mPulled new updates..." ; echo ;
 	sleep 5
