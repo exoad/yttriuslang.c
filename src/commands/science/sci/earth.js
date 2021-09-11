cf78ab42283539f6ac8fa0b5bef7108d3dc7205d
Jack Meng <error666jm@gmail.com>
2021-09-11 19:40:58 -0400

    recommit

---
+++ src/commands/science/sci/earth.js
@@ -0,0 +1,11 @@
+module.exports = {
+  config: {
+    name: `nullelement`,
+    category: "",
+    description: "",
+    aliases: [``],
+  },
+  run: async (bot, message, args) => {
+    message.channel.send(" .");
+  },
+};
