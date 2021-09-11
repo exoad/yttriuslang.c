a7b1f614b7e7485eb77caccb32d9b6d1f8d02a8f
Jack Meng <error666jm@gmail.com>
2021-09-11 09:25:52 -0400

    make build sh

---
+++ build/types.sh
@@ -0,0 +1,10 @@
+set -eu
+
+rm -rf generatedTypes
+tsc index.js --declaration --allowJs --emitDeclarationOnly --outDir generatedTypes
+cp src/harness/types/index.d.ts generatedTypes/src/harness/types/
+
+rm -rf imports/
+mkdir -p imports/
+echo "export * from '../src/tool/imports/lib';" > imports/index.d.ts
+echo "export {default} from '../src/tool/imports/lib';" >> imports/index.d.ts
