5388a4c3c5919c4cc757056070003e338775d24e
JACK MENG <error666jm@gmail.com>
2021-09-24 15:59:06 -0400

    update changelog command

--- src/commands/Testing/testing_list.js
+++ src/commands/Testing/testing_list.js
@@ -8,5 +8,6 @@ module.exports = {
   run: async (bot, message, args) => {
     
     let changeLog = "**CANARY Testing Commands v1.0**\nDescription: These commands are in testing and are not \"FINAL\"!!!\n```\ntesting_list```\n\n*The Yttrius Canary Program Copyright (C) 2021*";
+    message.channel.send(changeLog);
   },
 };
