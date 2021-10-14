a0e83387669c07f8f2a84863df0d92e2a00d29a1
exoad <jackmeng0814@gmail.com>
2021-10-14 13:32:35 +0000

    add fpv intro

--- harness/harness.c
+++ harness/harness.c
@@ -11,6 +11,8 @@ static_assert(__builtin_offsetof(struct token, len) == 8, "len=8");
 static_assert(__builtin_offsetof(struct token, line_no) == 12, "line_no=12");
 static_assert(__builtin_offsetof(struct token, type) == 16, "type=16");
 static_assert(__builtin_offsetof(struct token, special) == 20, "special=20");
+static_assert(__builtin_offsetof(struct token, fpv) == 34, "fpv=e4");
+
 
 int isdigit(int c) {
   return (c >= '0' && c <= '9');
