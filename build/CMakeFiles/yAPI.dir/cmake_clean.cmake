0f47ddd32a96ea8881c488fcc0169227132e9b2a
Jack Meng <jackmeng@gmail.com>
2022-07-16 18:55:10 -0400

    Add

---
+++ build/CMakeFiles/yAPI.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yAPI.dir/yAPI.c.o"
+  "CMakeFiles/yAPI.dir/yAPI.c.o.d"
+  "libyAPI.a"
+  "libyAPI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/yAPI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
