30cfed2d37dafa82f28f8db61005825f48e71598
Jack Meng <jackmeng0814@gmail.com>
2022-09-20 19:43:42 +0000

    `max` make default reg

--- tokens/try_dec_convert.hpp
+++ tokens/try_dec_convert.hpp
@@ -1,8 +1,4 @@
-// Copyright Antony Polukhin, 2016-2022.
-//
-// Distributed under the Boost Software License, Version 1.0. (See
-// accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
+
 
 #ifndef BOOST_STACKTRACE_DETAIL_TRY_DEC_CONVERT_HPP
 #define BOOST_STACKTRACE_DETAIL_TRY_DEC_CONVERT_HPP
--- tokens/windbg.cpp
+++ tokens/windbg.cpp
@@ -1,8 +1,4 @@
-// Copyright Antony Polukhin, 2016-2020.
-//
-// Distributed under the Boost Software License, Version 1.0. (See
-// accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
+
 
 #define BOOST_STACKTRACE_INTERNAL_BUILD_LIBS
 #define BOOST_STACKTRACE_LINK
