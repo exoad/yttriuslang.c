f9f45ceefaaad1323395629113dd579d57f470b3
Jack Meng <jackmeng0814@gmail.com>
2022-12-04 13:06:19 -0500

    ask is bad

--- include/auto_index.hpp
+++ include/auto_index.hpp
@@ -4,7 +4,6 @@
 // Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
-
 #ifndef BOOST_AUTO_INDEX_HPP
 #define BOOST_AUTO_INDEX_HPP
 
